9fc48b372a0db527c815306157ade69a0845141c
AdrianHarris96 <97938048+AdrianHarris96@users.noreply.github.com>
2023-04-15 21:18:39 -0400

    Re-organize comments - Needleman-Wunsch v1.0.1
    Preface each section of code with its comment. Add block comments

--- aharris334_nwAlign.py
+++ aharris334_nwAlign.py
@@ -2,12 +2,13 @@
 
 import sys
 
-filename1 = sys.argv[1]
-filename2 = sys.argv[2]
-
 #Example Input: ./<gtusername>_nwAlign.py seq1_nw.fa seq2_nw.fa
 #Code contains comments for print debugging. 
 
+filename1 = sys.argv[1]
+filename2 = sys.argv[2]
+
+#Simply open each fasta file and add seq to empty string.
 file1 = open(filename1, "r")
 seq1 = ""
 seq2 = ""
@@ -25,8 +26,8 @@ for line in file2:
   else:
     line = line.strip()
     seq2 += line
-#Simply open each fasta file and add seq to empty string.
 
+#Code does not work if the shorter sequence is used in vertical orientation. This ensures that does not happen. 
 if len(seq2) < len(seq1):
   tempseq = seq1
   seq1 = seq2
@@ -34,11 +35,14 @@ if len(seq2) < len(seq1):
 else:
   pass
 
-#Code does not work if the shorter sequence is used in vertical orientation. This ensures that does not happen. 
+
+#Scoring system
 match = 1
 mismatch = -1
 gap = -1
 
+#Creation of empty matrix.
+#Need this if I plan to move via loops row-by-row, populating the matrix later. 
 lengthSeq1 = len(seq1)+1
 lengthSeq2 = len(seq2)+1
 matrix = []
@@ -59,9 +63,13 @@ for x in range(lengthSeq2):
         newList.append(item)
   matrix.append(newList)
 
-#Creation of empty matrix.
-#Need this if I plan to move via loops row-by-row, populating the matrix later. 
-
+'''
+Populating the empty matrix from earlier.
+We first check horizontal sequence with the index [y-1] is equal to the vertical sequence with index[x-1]. Confusing, I know!
+If so, the diag value is the diagonal value + match value. If not, we say it is diagonal value + mismatch. 
+Up and left are always just gaps, so each time it iterates, it sets them to the value above or to the left plus the gap value, respectively. 
+Then, we fill up our old matrix with values and our newMatrix with "d", "u" or "l" with that priority using relational operators. 
+'''
 newMatrix = []
 for x in range(1, lengthSeq2):
   newList2 = []
@@ -83,12 +91,6 @@ for x in range(1, lengthSeq2):
       matrix[x][y] = left
   newMatrix.append(newList2)
 
-#Populating the empty matrix from earlier.
-#We first check horizontal sequence with the index [y-1] is equal to the vertical sequence with index[x-1]. Confusing, I know!
-#If so, the diag value is the diagonal value + match value. If not, we say it is diagonal value + mismatch. 
-#Up and left are always just gaps, so each time it iterates, it sets them to the value above or to the left plus the gap value, respectively. 
-#Then, we fill up our old matrix with values and our newMatrix with "d", "u" or "l" with that priority using relational operators. 
-
 #for row in matrix:
   #print(row)
 
@@ -100,13 +102,19 @@ for row in newMatrix:
   for item in row:
     count +=1
 
+#rowLength (i.e. number of columns) and length of newMatrix (i.e. number of rows).
+#-1 is done because we will use the range function in reverse, and we have to pay attention to indexing.
 rowLength = int(count / len(newMatrix))
-
 position1 = len(newMatrix) - 1
 position2 = rowLength - 1
 newString = str(newMatrix[position1][position2])
-#rowLength (i.e. number of columns) and length of newMatrix (i.e. number of rows).
-#-1 is done because we will use the range function in reverse, and we have to pay attention to indexing.
+
+'''
+Breaks are used so we do not continually iterate through the range functions. May not be necessary, considering I am continually setting the position1 and position2.
+newString is set from the start with whatever letter is in that corner of the matrix ("d", "u" or "l").
+Depending on what it is we will move diagonal, up or left by adjusting position1 and position2.
+newString is continually populated with each move. 
+'''
 for x in range((position1), -1, -1):
   for y in range((position2), -1, -1):
     if newMatrix[position1][position2] == "d":
@@ -123,19 +131,14 @@ for x in range((position1), -1, -1):
       newString += newMatrix[position1][position2] 
       break
 
-#Breaks are used so we do not continually iterate through the range functions. May not be necessary, considering I am continually setting the position1 and position2.
-#newString is set from the start with whatever letter is in that corner of the matrix ("d", "u" or "l").
-#Depending on what it is we will move diagonal, up or left by adjusting position1 and position2.
-#newString is continually populated with each move. 
-
 #print(newString)
-
-if (len(seq1)+len(seq2)) % 2 == 0:
-  newString += "d"
 
 #Not sure why, but my code falls apart without this. It could be the way I initially set up the matrices. 
 #It prevents it from ending on a "u" or "l".
+if (len(seq1)+len(seq2)) % 2 == 0:
+  newString += "d"
 
+#Converting string to list
 seq1List = []
 seq2List = []
 for x in range(len(seq1)):
@@ -143,11 +146,11 @@ for x in range(len(seq1)):
 for x in range(len(seq2)):
   seq2List.append(seq2[x])
 
-#Converting string to list
-
-length = len(newString)-1
 # Once both even and old combinations of sequences are made equal in terms of their combined length, this length variable above ensures no index error with the next loop. 
+length = len(newString)-1
 
+#Simply iterate through string, pop for both if diagonal or one element depending on if it is "u" or "l".
+#Using the insert() was messy, so I thought to populate two new list concurrently. 
 count = 0
 finalSeq1List = []
 finalSeq2List = []
@@ -163,12 +166,11 @@ for x in range(length):
     finalSeq1List.append(seq1List.pop())
     finalSeq2List.append("-")
 
-#Simply iterate through string, pop for both if diagonal or one element depending on if it is "u" or "l".
-#Using the insert() was messy, so I thought to populate two new list concurrently. 
-
 #print(finalSeq1List)
 #print(finalSeq2List)
 
+#The list should be the same length after the "pop"-loop, so I iterate using the length of one, and create another list, telling me if there is a match, mismatch or gap. 
+#We also create the score here.
 matchList = []
 score = 0
 
@@ -183,17 +185,11 @@ for x in range(len(finalSeq1List)):
     matchList.append("*")
     score += mismatch
 
-#The list should be the same length after the "pop"-loop, so I iterate using the length of one, and create another list, telling me if there is a match, mismatch or gap. 
-#We also create the score here.
-
-seq1String = ("".join(str(char) for char in finalSeq1List))
-
-matchString = ("".join(str(char) for char in matchList))
-
-seq2String = ("".join(str(char) for char in finalSeq2List))
-
 #Conversion back to strings, but remember we created newString earlier via backtracking. 
 #I have intentionally left these strings backwards.
+seq1String = ("".join(str(char) for char in finalSeq1List))
+matchString = ("".join(str(char) for char in matchList))
+seq2String = ("".join(str(char) for char in finalSeq2List))
 
 print(seq1String[::-1])
 print(matchString[::-1])
